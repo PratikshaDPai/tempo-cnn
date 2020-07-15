629176089e0146c31fd36ba1a187cd5e2442dadf
PratikshaDPai <41014833+PratikshaDPai@users.noreply.github.com>
2020-07-15 00:13:46 -0500

    Update and rename test_feature.py to test_feature1.py

--- tests/test_feature1.py
+++ tests/test_feature1.py
@@ -3,6 +3,7 @@ import unittest
 import librosa
 
 from tempocnn.feature import read_features
+from tempocnn.tester import demo_human_cochleagram_helper  #(signal, sr, n, sample_factor=2, downsample=None, nonlinearity=None)
 
 
 class TestTempoClassifier(unittest.TestCase):
@@ -15,10 +16,15 @@ class TestTempoClassifier(unittest.TestCase):
         # possible feature windows with half overlap
         num_feature_windows = (num_feature_frames // 128) // 2
 
-        features = read_features(file)
+        #features = read_features(file)
+
+
+        demo_stim, demo_sr = utils.wav_to_array(file)
+        demo_n = 38  # default filter for low_lim=50 hi_lim=20000, we use loww = 20, high = 5000
+        #demo_human_cochleagram(demo_stim, demo_sr, demo_n)
+        features= demo_human_cochleagram_helper(file, demo_sr, demo_n, sample_factor=2, downsample=None, nonlinearity=None)
         self.assertEqual(len(features.shape), 4)
         self.assertEqual(features.shape[0], num_feature_windows)
         self.assertEqual(features.shape[1], 40)
         self.assertEqual(features.shape[2], 256)
         self.assertEqual(features.shape[3], 1)
-
